d97dd990d57c6c5493afd29557013637ac37de37
beamyyl <128243705+beamyyl@users.noreply.github.com>
2026-03-06 20:15:25 +0200

    Update gentoo-systemd-uefi.sh

--- gentoo-systemd-uefi.sh
+++ gentoo-systemd-uefi.sh
@@ -57,10 +57,6 @@ emaint binhost --sync
 getuto
 emerge --sync --quiet
 
-# Mirrors
-emerge -qgv --oneshot app-portage/mirrorselect
-mirrorselect -i -o >> /etc/portage/make.conf
-
 # CPU flags
 emerge -gqv --oneshot app-portage/cpuid2cpuflags
 mkdir -p /etc/portage/package.use
