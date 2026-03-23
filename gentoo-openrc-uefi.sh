99de603dda7de54c7a74f5cc8414fb6fe3c0a85e
beamyyl <128243705+beamyyl@users.noreply.github.com>
2026-03-23 14:21:25 +0200

    Update gentoo-openrc-uefi.sh

--- gentoo-openrc-uefi.sh
+++ gentoo-openrc-uefi.sh
@@ -63,6 +63,7 @@ emerge -quDU --getbinpkg @world
 # Firmware & kernel
 mkdir -p /etc/portage/package.use
 echo 'sys-kernel/installkernel dracut' >> /etc/portage/package.use/00installkernel
+emerge -q sys-kernel/dracut sys-kernel/gentoo-sources
 emerge -q sys-kernel/linux-firmware sys-firmware/sof-firmware sys-kernel/gentoo-kernel-bin
 
 # Hostname + Essentials
