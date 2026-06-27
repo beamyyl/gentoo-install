d2d90950375f891d6461292c877b4d9610b67ee6
beamyyl <128243705+beamyyl@users.noreply.github.com>
2026-06-27 16:33:00 +0300

    Update a.sh

--- a.sh
+++ a.sh
@@ -2,5 +2,6 @@ emerge -q eselect-repository
 eselect repository enable Miezhiko
 emaint sync -r Miezhiko
 echo '*/*::Miezhiko ~amd64' > /etc/portage/package.accept_keywords/Miezhiko
-echo 'gnome-base/*::Miezhiko systemd -gnome-online-accounts'
+echo '*/gnome-settings-daemon input_devices_wacom' >> /etc/portage/package.use/gnome
+echo 'gnome-base/*::Miezhiko systemd -gnome-online-accounts' >> /etc/portage/package.use/gnome
 emerge -q gnome-shell gnome-control-center gnome-settings-daemon::Miezhiko
